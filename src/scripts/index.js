ad45c5a3cb9dc7fe155c257ffd99f14dc6526683
souhail404 <elabbouisouhail@gmail.com>
2023-03-07 22:12:54 +0100

    test d-f section responsivity

---
+++ src/scripts/index.js
@@ -0,0 +1,51 @@
+const banners = document.querySelectorAll('.hero-banner');
+const timeInterval  = 10000;
+setTimeout(moveRight , timeInterval)
+function moveRight(){
+    banners.forEach(banner => {
+        banner.style.transform = "translateX(-100%)";
+    });
+    setTimeout(moveLeft, timeInterval);
+};
+function moveLeft(){
+    banners.forEach(banner => {
+        banner.style.transform = "translateX(0%)";
+    });
+    setTimeout(moveRight, timeInterval);
+};
+
+
+// function nextBanner() {
+//   banner[currentBannerIndex].style.display = 'none';
+//   currentBannerIndex = (currentBannerIndex + 1) % banner.length;
+//   banner[currentBannerIndex].style.display = 'block';
+// }
+// Set the date 2 days from now
+const endDate = new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
+
+// Update the timer every second
+setInterval(() => {
+  // Get the time difference between the current date and the end date
+  const timeDiff = endDate - new Date();
+
+  // Calculate the remaining days, hours, minutes, and seconds
+  const days = Math.floor(timeDiff / (24 * 60 * 60 * 1000));
+  const hours = Math.floor((timeDiff % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+  const minutes = Math.floor((timeDiff % (60 * 60 * 1000)) / (60 * 1000));
+  const seconds = Math.floor((timeDiff % (60 * 1000)) / 1000);
+
+  // Display the remaining time in the HTML
+  document.getElementById("days").textContent = `${days} Day`;
+  document.getElementById("hours").textContent = `${hours} Hrs`;
+  document.getElementById("minutes").textContent = `${minutes} Min`;
+  document.getElementById("seconds").textContent = `${seconds} Sec`;
+}, 1000);
+
+
+
+
+
+
+
+
+
